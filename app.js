604a13ec761292310e16af9311e0f61b464df98e
Ali Raza <mrakaraza@gmail.com>
2023-03-13 21:55:06 +0100

    keyboard

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ let ilm = document.getElementById("ilm");
 let col = document.getElementById("col");
 let sco = document.getElementById("score");
 let input = document.getElementById("myinput");
+let inputDiv = document.querySelector('#no-outline');
 let score = 1;
 let collection = [];
 let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 input.addEventListener ("click", (e) => {
+    ilm.style.display = 'block';
     input.style.display = 'none';
+    inputDiv.style.display = 'none';
 } )
 function ran() {
     return str.charAt(Math.floor(Math.random() * 25));
@@ -28,5 +31,8 @@ let letter = ran();
           new Audio(`resources/sounds/${letter}.mp3`).play()
           
         
-	}
+	} else {console.log(evt.key.charAt(0).toUpperCase())
+
+        new Audio(`resources/sounds/${evt.key.charAt(0).toUpperCase()}.mp3`).play()
+    }
 });
